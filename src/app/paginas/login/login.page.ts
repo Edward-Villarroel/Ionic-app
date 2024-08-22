a8b5022ae7ca81c22d20271b4b0c8cc297e33019
Vina del Mar <vina@duoc.acad>
2024-08-22 14:00:49 -0400

    toast notification

--- src/app/paginas/login/login.page.ts
+++ src/app/paginas/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -6,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+   nombre:string=""
+   usuario:string=""
+   contrasena:string=""
+  constructor(public mensaje:ToastController) { }
+   
 
-  constructor() { }
+    async mensajeExito(){
+        const toast= await this.mensaje.create({
+          message:'inicio de sesion exitoso',
+          duration:2000
+        });
+        toast.present();
 
+    }
+    ingresar(){
+      if(this.usuario ==="asd"&& this.contrasena==="1234"){
+        
+        console.log("inicio exitoso")
+        
+      }
+       else{
+        console.log("informacion erronea")
+       }
+   }
   ngOnInit() {
   }
 
